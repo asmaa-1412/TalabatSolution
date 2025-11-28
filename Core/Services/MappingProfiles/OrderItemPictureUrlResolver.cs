67b06524da57feff77e0a68aa289cae33ce48de6
asmaa <ae2297@fayoum.edu.eg>
2025-11-28 23:39:08 +0200

    Part03 - OrderMapping

---
+++ Core/Services/MappingProfiles/OrderItemPictureUrlResolver.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using DomainLayer.Models.OrderModels;
+using Microsoft.Extensions.Configuration;
+using Shared.Dtos.OrderDtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServicesLayer.MappingProfiles
+{
+    public class OrderItemPictureUrlResolver(IConfiguration _configuration) : IValueResolver<OrderItem, OrderItemDto, string>
+    {
+        public string Resolve(OrderItem source, OrderItemDto destination, string destMember, ResolutionContext context)
+        {
+            if (string.IsNullOrWhiteSpace(source.Product.PictureUrl))
+                return string.Empty;
+            else
+            {
+                var url = $"{_configuration.GetSection("Urls")["BaseUrl"]}{source.Product.PictureUrl}";
+                return url;
+            }
+
+        }
+
+    }
+}
--- Core/Services/MappingProfiles/OrderMappingProfile.cs
+++ Core/Services/MappingProfiles/OrderMappingProfile.cs
@@ -1,6 +1,7 @@
 ﻿using AutoMapper;
 using DomainLayer.Models.OrderModels;
 using Shared.Dtos.IdentityDto;
+using Shared.Dtos.OrderDtos;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +15,17 @@ namespace ServicesLayer.MappingProfiles
         public OrderMappingProfile()
         {
             CreateMap<AddressDto, OrderAddress>().ReverseMap();
+
+            CreateMap<Order,OrderToReturnDto>()
+                .ForMember(dest=>dest.DeliveryMethod,opt=>opt
+                .MapFrom(src=>src.DeliveryMethod.ShortName));
+
+            CreateMap<OrderItem, OrderItemDto>()
+               .ForMember(dest => dest.ProductName, opt => opt
+               .MapFrom(src => src.Product.ProductName))
+               .ForMember(dest => dest.PictureUrl, opt => opt
+               .MapFrom<OrderItemPictureUrlResolver>());
+
         }
     }
 }
